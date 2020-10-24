98d38770d69d84203cb362e9d54db881f4b5384e
TieNan2019 <53027948+TieNan2019@users.noreply.github.com>
2020-10-24 20:04:13 +0800

    Create 235_二叉搜索树的最近公共祖先.cpp

---
+++ 235_二叉搜索树的最近公共祖先.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+        TreeNode *traverse(TreeNode * root, 
+        TreeNode *p, TreeNode *q) {
+                if (root == NULL)
+                        return NULL;
+
+                // 两个值全部在左枝
+                if (root->val > p->val
+                && root->val > q->val)
+                        return traverse(root->left, p, q);
+
+                // 两个值全部在右枝
+                else if (root->val < p->val
+                && root->val < q->val)
+                        return traverse(root->right, p, q);
+
+                return root;
+        }
+
+
+        TreeNode* lowestCommonAncestor(
+        TreeNode* root, TreeNode* p, TreeNode* q)
+        {
+                return traverse(root, p, q);
+        }
+};
