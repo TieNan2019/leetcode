02240e9d34fdc027a2a7dd3025d25307ad9ca85a
TieNan2019 <53027948+TieNan2019@users.noreply.github.com>
2020-11-11 10:49:19 +0800

    Create 514_自由之路.cpp

---
+++ 514_自由之路.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+        map<int, int> rec;
+        int recursion(string ring, string key) {
+                if (key.empty())
+                        return 0;
+
+                char p = ring.front();
+                int i = 0;
+                /* 右旋 */
+                /* 要按下按键，所以至少有一步 */
+                int toRight = 1;
+                while (p != key.front()) {
+                        i++;
+
+                        if (i == ring.size()) {
+                                toRight = INT_MAX;
+                                break;
+                        }
+
+                        p = ring[i];
+                        toRight++;
+                }
+
+                int j = ring.size() - 1;
+                p = ring.back();
+                /* 要提前往左走一个，还要按下按键，所以是两步 */
+                int toLeft = 2;
+                while (p != key.front()) {
+                        j--;
+                        if (j < 0) {
+                                toLeft = INT_MAX;
+                                break;
+                        }
+
+                        p = ring[j];
+                        toLeft++;
+                }
+
+                int leftRes = INT_MAX, rightRes = INT_MAX;
+                if (toLeft != INT_MAX) {
+                        /* 向左转的情况 */
+                        /* 更新轮子的状态 */
+                        string newRing = ring.substr(j) + ring.substr(0, j);
+
+                        leftRes = toLeft + recursion(newRing, key.substr(1));
+                }
+
+                
+                if (toRight != INT_MAX) {
+                        /* 向右转的情况 */
+                        string newRing = ring.substr(i) + ring.substr(0, i);
+
+                        rightRes = toRight + recursion(newRing, key.substr(1));
+                }
+
+                return leftRes < rightRes ? leftRes : rightRes;
+        }
+
+        int findRotateSteps(string ring, string key) {
+
+                return recursion(ring, key);
+        }
+};
