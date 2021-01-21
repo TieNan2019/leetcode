cb41c6032825e7912f5460760339717ed7e2308e
TieNan2019 <53027948+TieNan2019@users.noreply.github.com>
2021-01-21 14:57:37 +0800

    Update 剑指 Offer 68 - II_ 二叉树的最近公共祖先.cpp

--- 剑指 Offer 68 - II_ 二叉树的最近公共祖先.cpp
+++ 剑指 Offer 68 - II_ 二叉树的最近公共祖先.cpp
@@ -11,12 +11,12 @@ class Solution {
 public:
         TreeNode *pNode;
 
-        TreeNode *recursion(TreeNode *root, const TreeNode* p, const TreeNode *q) {
+        bool recursion(TreeNode *root, const TreeNode* p, const TreeNode *q) {
                 if (root == NULL)
                         return root;
                 
-                TreeNode *lret = recursion(root->left, p, q);
-                TreeNode *rret = recursion(root->right, p, q);
+                bool lret = recursion(root->left, p, q);
+                bool rret = recursion(root->right, p, q);
                 
                 /* 后序遍历 */
                 if (pNode == NULL /* 父节点还没有被找到的时候才会进行判断，因为要求找到最小父节点 */
@@ -25,18 +25,11 @@ public:
                 ))
                         pNode = root;
 
-                /* 发现当前节点时，返回非空，像当前节点的父节点报告 */
-                if (root == p || root == q)
-                        return root;
-
+                /* 发现当前节点时，返回非空，向当前节点的父节点报告 */
+                return (root == p || root == q)
                 /* 如果某一个子节点是目标节点，则继续向上报告 */
-                if (lret)
-                        return lret;
-                if (rret)
-                        return rret;
-
                 /* 如果当前节点并非目标节点，且子节点也不存在目标节点，则返回空告诉上层节点 */
-                return NULL;
+                || lret || rret;
         }
 
         TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
