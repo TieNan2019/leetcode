039707b40dcf936fd0cc49bc5b2b2f3bfb9cba8e
TieNan2019 <53027948+TieNan2019@users.noreply.github.com>
2021-02-26 13:41:36 +0800

    Create 590. N叉树的后序遍历_MyMethod.cpp

---
+++ 590. N叉树的后序遍历_MyMethod.cpp
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+        vector<int> postorder(Node* root) {
+                if (root == nullptr)
+                        return {};
+        
+                vector<int> ans;
+
+                Node *node = root;
+                stack<Node *> s;
+                s.push(root);
+                stack<int> sidx;
+                sidx.push(0);
+
+
+                int cnt = 0;
+                while (!s.empty()) {
+                        if (cnt < s.top()->children.size()) {
+                                node = s.top()->children[cnt];
+
+                                s.push(node);
+                                sidx.push(cnt);
+
+                                cnt = 0;
+                        }
+                        else {
+                                /* 后序遍历发生在此处 */
+                                ans.push_back(s.top()->val);
+
+                                if (cnt + 1 >= s.top()->children.size()) {
+                                        s.pop();
+
+                                        cnt = sidx.top() + 1;
+                                        sidx.pop();
+                                }
+                                else
+                                        cnt++;
+                        }
+                }
+
+                return ans;
+        }
+};
